daf1779b3af72949913be822f4c7d5aca801bb86
Daniel Blanco <74727652+dabadrian@users.noreply.github.com>
2023-05-21 23:43:47 -0400

    Update db.js for render.com deployment

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 // Configura la conexión a la base de datos PostgreSQL
 const pool = new Pool({
   user: 'dbadmin',
-  host: '192.168.1.110',
+  host: 'postgres://dbadmin:783sJp3IUdMzQ3QTg9zdFb8FIzj80cif@dpg-chle1lbhp8uej74j47fg-a/proyecto_api',
   database: 'proyecto_api',
-  password: 'dbadmin',
+  password: '783sJp3IUdMzQ3QTg9zdFb8FIzj80cif',
   port: 5432,
 });
 
